22926f26cafd9656871e0356ebd6b6929685a069
Narasimharaj C <simhanature@gmail.com>
2024-02-17 15:12:42 -0800

    Update Queue.swift - convert to struct and cleanup

--- Queue.swift
+++ Queue.swift
@@ -1,67 +1,104 @@
-import Foundation
-
+//Node of queues
 class Node<T> {
     
+    //Properties
     var val: T?
-    var _tail: Node<T>?
-    var _head: Node<T>?
+    //var _prev: Node<T>?
     var _next: Node<T>?
-    var _prev: Node<T>?
-    var _iteratorPoint: Node<T>?
     
     init(v: T) {
         val = v
     }
     
-    func add(item: T) {
-        var node = Node(v: item)
+}
+
+//Queue
+struct Queue<T> {
+    
+    //properties
+    var _tail: Node<T>?
+    var _head: Node<T>?
+    var _iteratorPoint: Node<T>?
+        
+    mutating func enqueue(item: T) {
+        let node = Node(v: item)
         if _head == nil {
             _head = node
             _tail = node
         } else {
-            _tail?._prev = node
-            node._next = _tail
+            _tail?._next = node
+            //node._prev = _tail
             _tail = node
         }
     }
     
-    func pop() -> T? {
-        let popValue = _head?.val
-        _head = _head?._prev
-        return popValue
+    mutating func dequeue() -> T? {
+        let dequeueValue = _head?.val
+        _head = _head?._next
+        return dequeueValue
     }
     
-    func peak() -> T? {
+    func peek() -> T? {
         return _head?.val
     }
     
-    func printVal() -> String? {
-        return String(val as? Int ?? 0)
+    static func printElements(queue: Queue) {
+        var elems: [T] = []
+        for x in queue {
+            elems.append(x)
+        }
+        print(elems)
     }
+
 }
 
-//Add Iterator
-extension Node: IteratorProtocol, Sequence {
-    func next() -> T? {
+//enqueue iteration
+extension Queue: IteratorProtocol, Sequence {
+    mutating func next() -> T? {
         if _iteratorPoint == nil {
             _iteratorPoint = _head
             return _head?.val
         } else {
-            _iteratorPoint = _iteratorPoint?._prev
+            _iteratorPoint = _iteratorPoint?._next
             return _iteratorPoint?.val
         }
     }
 }
 
-//Input via Array
+//Create Queue
+var queue = Queue<Int>()
+//Input values
 var inputArr = Array(0...9)
-let queue = Node(v: 0)
 for x in inputArr {
-   queue.add(item: x)
+   queue.enqueue(item: x)
 }
-print("---------")
 
-//Print via Iterator
-for x in queue {
-    print("x: ", x)
-}
+print("\n----Print Elements-----")
+Queue.printElements(queue: queue)
+
+print("\n---dequeue 3 elements-----")
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+
+print("\n---do a peek-----")
+print(queue.peek())
+
+print("\n----Print Elements-----")
+Queue.printElements(queue: queue)
+
+print("\n----enqueue 3 Elements-----")
+queue.enqueue(item: 101)
+queue.enqueue(item: 102)
+queue.enqueue(item: 103)
+
+print("\n----Print Elements-----")
+Queue.printElements(queue: queue)
+
+print("\n---dequeue 3 elements-----")
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+
+print("\n----Print Elements-----")
+Queue.printElements(queue: queue)
