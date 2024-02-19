084e27ad5052a9ef015e8080b36c59afb0d337e0
Narasimharaj C <simhanature@gmail.com>
2024-02-19 12:39:12 -0800

    Update primes.swift - added swizzling to measure time

--- numbers/primes.swift
+++ numbers/primes.swift
@@ -1,67 +1,99 @@
 import Foundation
 
-var primes: [Int] = []
-
-//simple prime func // best for single no.
-func isPrime(num: Int) -> Bool {
-    if num < 2 {
-        return false
-    }
-    if num == 2 {
-        return true
-    }
+class Prime {
     
-    //floor is okay, ceil is not required but for 3. range becomes (2...1)
-    let sqrtNum = Int(ceil(sqrt(Double(num))))
-    for i in 2...sqrtNum {
-        if num % i == 0 {
+    var primes: [Int] = []
+    
+    //simple prime func // best for single no.
+    @objc dynamic func isPrime(num: Int) -> Bool {
+        if num < 2 {
             return false
         }
-    }
-    
-    return true
-}
-
-//best for generating list by makng use of only primes generated already
-func isPrimeN(num: Int) -> Bool {
-    if num < 2 {
-        return false
-    }
-    if num == 2 {
-        return true
-    }
-    
-    let sqrtNum = Int(ceil(sqrt(Double(num))))
-    for val in primes {
-        if val > sqrtNum {
+        if num == 2 {
             return true
         }
-        if num % val == 0 {
+        
+        //floor is okay, ceil is not required but for 3. range becomes (2...1)
+        let sqrtNum = Int(ceil(sqrt(Double(num))))
+        for i in 2...sqrtNum {
+            if num % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    //best for generating list by makng use of only primes generated already
+    func isPrimeN(num: Int) -> Bool {
+        if num < 2 {
             return false
         }
-    }
-    return true
-}
-
-//Generate all prime numbers under the given limit
-func primesUnder(n: Int) {
-    for i in 2..<n {
-        if isPrimeN(num: i) {
-            primes.append(i)
+        if num == 2 {
+            return true
         }
+        
+        let sqrtNum = Int(ceil(sqrt(Double(num))))
+        for val in primes {
+            if val > sqrtNum {
+                return true
+            }
+            if num % val == 0 {
+                return false
+            }
+        }
+        return true
     }
-    print("for under:", n)
-    print("all primes:", primes)
-    print("primes count: ", primes.count)
+    
+    //Generate all prime numbers under the given limit
+    func primesUnder(n: Int) {
+        for i in 2..<n {
+            if isPrimeN(num: i) {
+                primes.append(i)
+            }
+        }
+        print("for under:", n)
+        print("all primes:", primes)
+        print("primes count: ", primes.count)
+    }
+    
+    func run() {
+        //Check 1 prime
+        print(isPrime(num: 67280421310721))
+        //
+        let date = Date()
+        primesUnder(n: 100)
+        print("time taken: ", Date().timeIntervalSince1970 - date.timeIntervalSince1970)
+    }
+    
+    
 }
 
-//Check 1 prime
-let date1 = Date()
-print(isPrime(num: 67280421310721))
-print("time taken to check ths prime: ", Date().timeIntervalSince1970 - date1.timeIntervalSince1970)
+extension Prime {
+    
+    @objc func tracked_isPrime(num: Int) -> Bool {
+        //Check 1 prime
+        let date1 = Date()
+        defer {
+            print("swizzle: time taken: ", Date().timeIntervalSince1970 - date1.timeIntervalSince1970)
+        }
+        return tracked_isPrime(num: num)
+    }
+       
+    static func swizzle() {
+        let originalSelector = #selector(Prime.isPrime(num:))
+        let swizzledSelector = #selector(Prime.tracked_isPrime(num:))
+        
+        let originalMethod = class_getInstanceMethod(self, originalSelector)
+        let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+        method_exchangeImplementations(originalMethod!, swizzledMethod!)
+    }
+                 
+}
 
-//Get all primes under given number.
-let date = Date()
-primesUnder(n: 200000)
-print("time taken: ", Date().timeIntervalSince1970 - date.timeIntervalSince1970)
+//
+//Initiate Swizzle
+Prime.swizzle()
 
+//Run test
+let p = Prime()
+p.run()
