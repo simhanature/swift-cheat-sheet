24f7a186519c22770442beee06b5bc80d082b566
Narasimharaj C <simhanature@gmail.com>
2024-02-29 15:39:55 -0800

    Update the Binary tree & added depth first

--- data-structure/BinaryTree.swift
+++ data-structure/BinaryTree.swift
@@ -1,5 +1,5 @@
 //Allow T to be Equatable so that you can identify the nodes
-//This is made to be Binary Tree
+//This node is made to serve the purpose as Binary Tree
 class Node<T: Equatable>: Any {
     
     //Variables
@@ -8,12 +8,14 @@ class Node<T: Equatable>: Any {
     var right: Node<T>?
     var count: Int = 0
     
+    //parent node reference
     var parent: Node<T>? {
         didSet {
             parent?.incrementCount()
         }
     }
     
+    //check if node has 1 or more childred
     var hasChildren: Bool {
         return right != nil || left != nil
     }
@@ -23,7 +25,7 @@ class Node<T: Equatable>: Any {
         self.val = val
     }
     
-    //Get the height
+    //Get the height to the node's position
     public func height() -> Int {
         if left == nil && right == nil  {
             return 1
@@ -57,6 +59,13 @@ class Node<T: Equatable>: Any {
         }
     }
     
+    //Helper functions
+    //Swap nodes by value
+    private func swapNodes(node1: Node<T>, node2: Node<T>) {
+        (node1.val, node2.val) = (node2.val, node1.val)
+    }
+    
+    //Mark: Remove
     private func execNodeRemoval(node: Node<T>) {
         let parent = node.parent
         if parent?.left == node {
@@ -65,11 +74,6 @@ class Node<T: Equatable>: Any {
             parent?.right = nil
         }
         node.parent = nil
-    }
-    
-    //Swap nodes bu value
-    private func swapNodes(node1: Node<T>, node2: Node<T>) {
-        (node1.val, node2.val) = (node2.val, node1.val)
     }
     
     //
@@ -92,10 +96,10 @@ class Node<T: Equatable>: Any {
         }
        return nil
     }
-
-    //find the node
+    
+    //Mark: find a node
     func find(node: Node<T>) -> Node<T>? {
-        if node.val == self.val {
+        if node == self {
             return self
         } else {
             if let lNode = left?.find(node: node) {
@@ -109,7 +113,8 @@ class Node<T: Equatable>: Any {
         }
     }
     
-    ///Make Printable
+    //Mark: Print
+    //Add printable
     public func printableVal() -> String {
         if let x = val as? Int {
             return String(x)
@@ -117,47 +122,93 @@ class Node<T: Equatable>: Any {
             return "undefined"
         }
     }
-
-    //print by levels
-    func printByLevels(node: Node<T>, level: Int, elems: inout [String]) {
-        if level == 0 {
-            elems.append(node.printableVal() + "<=" + (node.parent?.printableVal() ?? "root"))
-        } else if level > 0 {
-            if let lLeft = node.left {
-                self.printByLevels(node: lLeft, level: level - 1, elems: &elems)
-            }
-            if let lRight = node.right {
-                self.printByLevels(node: lRight, level: level - 1, elems: &elems)
-            }
-        }
-    }
     
-    ///Print path for element
+    //Print path for this element
     func printPath() {
         var path: String = String((self.val as? Int) ?? 0)
         var par = self.parent
         while par != nil {
-            path += "<-" + String((par?.val as? Int) ?? 0)
+            path += " <- " + String((par?.val as? Int) ?? 0)
             par = par?.parent
         }
         print("Path of \(self.val): \(path)")
+    }
+    
+    
+    
+    //Mark: Depth first traversal
+    //#1. Preorder
+    func getPreorder(node: Node, elems: inout [String]) {
+        elems.append(node.printableVal())
+        if let leftNode = node.left{
+            getPreorder(node: leftNode, elems: &elems)
+        }
+        if let rightNode = node.right{
+            getPreorder(node: rightNode, elems: &elems)
+        }
+    }
+    
+    func printPreorder() {
+        var elems = [String]()
+        getPreorder(node: self, elems: &elems)
+        print("preorder:", elems)
+    }
+    
+    //#2. Inorder
+    func getInorder(node: Node, elems: inout [String]) {
+        if let leftNode = node.left{
+            getInorder(node: leftNode, elems: &elems)
+        }
+        elems.append(node.printableVal())
+        if let rightNode = node.right{
+            getInorder(node: rightNode, elems: &elems)
+        }
+    }
+    
+    func printInorder() {
+        var elems = [String]()
+        getInorder(node: self, elems: &elems)
+        print("inorder:", elems)
+    }
+    
+    //#3. Postorder
+    func getPostorder(node: Node, elems: inout [String]) {
+        if let leftNode = node.left{
+            getPostorder(node: leftNode, elems: &elems)
+        }
+        if let rightNode = node.right{
+            getPostorder(node: rightNode, elems: &elems)
+        }
+        elems.append(node.printableVal())
+    }
+    
+    func printPostorder() {
+        var elems = [String]()
+        getPostorder(node: self, elems: &elems)
+        print("postorder:", elems)
+    }
+
+    //Mark: traversal by breadth
+    func getByLevels(level: Int, elems: inout [String]) {
+        if level == 0 {
+            elems.append(self.printableVal() + " <= " + (self.parent?.printableVal() ?? "root"))
+        } else if level > 0 {
+            if let lLeft = self.left {
+                lLeft.getByLevels(level: level - 1, elems: &elems)
+            }
+            if let lRight = self.right {
+                lRight.getByLevels(level: level - 1, elems: &elems)
+            }
+        }
     }
     
     ///Print tree as readable
     func printByBreadth() {
         for x in 0..<self.height() {
             var elems: [String] = []
-            tree.printByLevels(node: tree, level: x, elems: &elems)
+            self.getByLevels(level: x, elems: &elems)
             print("level: \(x)")
             print("elements: \(elems)")
-        }
-    }
-    
-    ///Generate Tree from Array
-    func generate(arr: [T]) {
-        for x in arr {
-            var node: Node<T> = Node(val: x)
-            add(node: node)
         }
     }
     
@@ -172,26 +223,54 @@ extension Node: Equatable {
     }
 }
 
+struct BinaryTree<T: Equatable> {
+    
+    var root: Node<T>?
+    
+    ///Generate Tree from Array
+    mutating func generate(arr: [T]) {
+        var arr = arr
+        guard arr.count > 0 else {
+            return
+        }
+        self.root = Node(val: arr.removeFirst())
+        for x in arr {
+            self.root?.add(node: Node(val: x))
+        }
+    }
+    
+    ///Print tree as readable
+    func printByBreadth() {
+        root?.printByBreadth()
+    }
+    
+    func printPreorder() {
+        root?.printPreorder()
+    }
+    
+    func printInorder() {
+        root?.printInorder()
+    }
+    
+    func printPostorder() {
+        root?.printPostorder()
+    }
+    
+}
 
 //generate elements
 var inputArr = Array((0...31))
 
 //construct tree
-let tree = Node(val: inputArr.removeFirst())
+var tree = BinaryTree<Int>()
 tree.generate(arr: inputArr)
 
-//print tree height and count
-print("height: ", tree.height())
-print("count: ", tree.count)
-
-//print tree
+print("\r\n---By breadth traversal-------")
+//By breadth traversal
 tree.printByBreadth()
 
-//Find and print path of a node
-let myNode = tree.find(node: Node(val: 14))
-myNode?.printPath()
-let removedNode = tree.remove(node: Node(val: 14))
-print("removed Node: ", removedNode?.printableVal()) //Returned node value is not correct. need to check
-myNode?.printPath()
-tree.printByBreadth()
-
+print("\r\n---By depth traversal-------")
+//By depth traversal
+tree.printPreorder()
+tree.printInorder()
+tree.printPostorder()
